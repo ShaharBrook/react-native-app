9faeb89366a1f5088ea4ac4692a64c8f95651b76
Shahar <bro.shahar@gmail.com>
2019-03-07 15:07:39 +0200

    removing react-native-paper unused package

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-import * as React from 'react';
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
 import App from './src/app/app';
 
 export default function Main() {
   return (
-    <PaperProvider>
       <App />
-    </PaperProvider>
   );
 }
 
