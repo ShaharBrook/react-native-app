b24bcb526e3a1fdedc4a99cfc41e501edaba896e
Shahar <bro.shahar@gmail.com>
2019-03-07 15:52:49 +0200

    adding some scroll

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import CardsContainer from '../cards-container/cardsContainer';
 
 export default class App extends React.Component {
   render() {
     return (
       <View styles={styles.container}>
-        <CardsContainer/>
+        <ScrollView>
+          <CardsContainer/>
+        </ScrollView>
       </View>
     );
   }
